2b11259d6bac31186239ff843bcc0a04a9f55ae0
Bymba <bymba@Bymbas-MacBook-Air.local>
2026-04-14 20:32:55 +0800

    Updated

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -25,13 +25,12 @@
 
 #include "raylib.h"
 #include "screens.h"
-#include <stdbool.h>
 
 //----------------------------------------------------------------------------------
 // Module Variables Definition (local)
 //----------------------------------------------------------------------------------
-static float screenWidth = GetScreenWidth();
-static float screenHeight = GetScreenHeight();
+static float screenWidth = 0.0f;
+static float screenHeight = 0.0f;
 static float delta = 0.0f;
 
 static int framesCounter = 0;
@@ -43,14 +42,14 @@ typedef struct
     Rectangle rec;
     Vector2 velocity;
     float maxSpeed;
-    bool normalized;
 } Player;
 
 static Player player = { 0 };
 
-void normalizeVector2(Vector2 *v)
+static void normalizeVector2(Vector2 *v, float speed)
 {
-    v->x /= 3.14; v->y /= 3.14;
+    v->x = (v->x > 0) ? speed / 1.4142f : -speed / 1.4142f;
+    v->y = (v->y > 0) ? speed / 1.4142f : -speed / 1.4142f;
 }
 
 //----------------------------------------------------------------------------------
@@ -61,6 +60,8 @@ void normalizeVector2(Vector2 *v)
 void InitGameplayScreen(void)
 {
     // TODO: Initialize GAMEPLAY screen variables here!
+    screenWidth = GetScreenWidth();
+    screenHeight = GetScreenHeight();
     delta = 0.0f;
 
     framesCounter = 0;
@@ -68,7 +69,7 @@ void InitGameplayScreen(void)
 
     player = (Player){
         (Rectangle){screenWidth/2+25, screenHeight/2+25, 50, 50},
-        (Vector2){0, 0}, 500, false
+        (Vector2){0, 0}, 300
     };
 }
 
@@ -79,31 +80,31 @@ void UpdateGameplayScreen(void)
     delta = GetFrameTime();
 
     // Player Update
-    player.rec.x += player.velocity * delta;
-    player.rec.y += player.velocity * delta;
+    player.rec.x += player.velocity.x * delta;
+    player.rec.y += player.velocity.y * delta;
 
     if (IsKeyDown(KEY_D)) player.velocity.x = player.maxSpeed;
     else if (IsKeyDown(KEY_A)) player.velocity.x = -player.maxSpeed;
-    else { player.velocity.x = 0; player.normalized = false; }
-
+    else player.velocity.x = 0;
+        
     if (IsKeyDown(KEY_W)) player.velocity.y = -player.maxSpeed;
     else if (IsKeyDown(KEY_S)) player.velocity.y = player.maxSpeed;
-    else { player.velocity.y = 0; player.normalized = false; }
+    else player.velocity.y = 0;
 
     // Normalize Player velocity
-    if (!player.normalized && (player.velocity.x && player.velocity.y))
+    if ((IsKeyDown(KEY_D) || IsKeyDown(KEY_A)) && (IsKeyDown(KEY_S) || IsKeyDown(KEY_W)))
     {
-        normalizeVector2(&player.velocity);
-        player.normalized = true;
+        normalizeVector2(&player.velocity, player.maxSpeed);
     }
 
+    // Screen border limit
     if (player.rec.x <= 0) player.rec.x = 0;
-    else if (player.rec.x + player.rec.width >= GetScreenWidth())
-        player.rec.x = GetScreenWidth() - player.rec.width;
+    else if (player.rec.x + player.rec.width >= screenWidth)
+        player.rec.x = screenWidth - player.rec.width;
 
     if (player.rec.y <= 0) player.rec.y = 0;
     else if (player.rec.y + player.rec.height >= screenHeight)
-        player.rec.y = screenHeight + player.rec.height;
+        player.rec.y = screenHeight - player.rec.height;
 
     if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
     {
